899974e24343f4028b0fd782d1adea3052242e89
Reuben G <rtg2@uw.edu>
2018-12-08 17:49:03 -0800

    Contour stuff

--- scripts/ApplyFilter.py
+++ scripts/ApplyFilter.py
@@ -19,8 +19,57 @@ class Filter:
         self.pub_red = rospy.Publisher(pub_topic_red, Image, queue_size=1)
         self.pub_blue = rospy.Publisher(pub_topic_blue, Image, queue_size=1)
         self.bridge = CvBridge()
-    
+
     def apply_filter_cb(self, msg):
+        in_image = None
+        try:
+            in_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+        except CvBridgeError as e:
+            print(e)
+        img = cv2.cvtColor(in_image, cv2.COLOR_BGR2HSV)
+
+        # Our operations on the frame come here
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # blur = cv2.GaussianBlur(gray,(5,5),0)
+        # ret, thresh_img = cv2.threshold(blur,91,255,cv2.THRESH_BINARY)
+
+        # define range of blue color in HSV
+        lower_blue = np.array([95, 105, 20])
+        upper_blue = np.array([115, 255, 255])
+
+        # mask for the colors
+        mask_blue = cv2.inRange(img, lower_blue, upper_blue)
+
+        thresh = cv2.threshold(mask_blue, 127, 255, cv2.THRESH_BINARY)[1]
+        #    thresh = cv2.threshold(mask_blue,60,255,cv2.THRESH_BINARY)[1]
+
+        cnts = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+        if len(cnts) > 5:
+            maxcontour = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(maxcontour)
+            center = (int(x), int(y))
+            a = np.arctan2(x - 320, 480 - y) * 180 / np.pi
+
+            print 'angle: ', a
+
+            # only proceed if the radius meets a minimum size
+            if radius > 10:
+                # draw the circle and centroid on the frame, then update the list of tracked points
+                cv2.circle(in_image, center, int(radius), (0, 255, 255), 2)
+                cv2.circle(in_image, center, 5, (0, 0, 255), -1)
+
+        for c in cnts:
+            cv2.drawContours(in_image, [c], -1, (0, 255, 0), 1)  # draws the Conture lines
+            cv2.drawContours(thresh, [c], -1, (0, 255, 0), 1)
+        # Display the resulting frame
+        try:
+            # self.pub_red.publish(self.bridge.cv2_to_imgmsg(frame, encoding="passthrough"))
+            self.pub_blue.publish(self.bridge.cv2_to_imgmsg(in_image, encoding="passthrough"))
+        except CvBridgeError as e:
+            print e
+
+    def apply_filter_cb_old(self, msg):
         try:
             in_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
         except CvBridgeError as e:
